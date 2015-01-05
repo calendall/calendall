9265cc6926c0424689c3f98944525d52724fb770
Xabier Larrakoetxea <slok69@gmail.com>
2015-01-05 21:07:33 +0100

    calendall/tasks#4 Add admin for user

--- calendall/profiles/admin.py
+++ calendall/profiles/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import CalendallUser
+
+
+class CalendallUserAdmin(admin.ModelAdmin):
+    list_display = ("id", "email", "username", "first_name", "last_name",
+                    "is_superuser")
+    readonly_fields = ("reset_token",)
+
+admin.site.register(CalendallUser, CalendallUserAdmin)
