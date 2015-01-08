781736e5fbb06e50f6ed8a96b73c960b1418da87
Xabier Larrakoetxea <slok69@gmail.com>
2015-01-08 09:41:32 +0100

    [calendall/tasks#26] Set template registration structure

--- calendall/profiles/views.py
+++ calendall/profiles/views.py
@@ -1,5 +1,11 @@
 from django.views.generic import CreateView
+from django.core.urlresolvers import reverse_lazy
+
+from .models import CalendallUser
 
 
 class CalendallUserCreate(CreateView):
-    pass
+    model = CalendallUser
+    fields = ['username', 'email', 'password']
+    template_name = "profiles/profiles_calendalluser_create.html"
+    success_url = reverse_lazy('profiles:calendalluser_create')
