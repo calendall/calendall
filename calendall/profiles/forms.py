c06ce34d008692f00854acf1436f1d8585c48181
Xabier Larrakoetxea <slok69@gmail.com>
2015-01-12 17:29:50 +0100

    calendall/tasks#26 Refactor

--- calendall/profiles/forms.py
+++ calendall/profiles/forms.py
@@ -1,10 +1,11 @@
-import re
 import logging
 
 from django import forms
 from django.utils.translation import ugettext_lazy as _
 
 from .models import CalendallUser
+from . import utils
+
 
 log = logging.getLogger(__name__)
 
@@ -24,7 +25,7 @@ class CalendallUserCreateForm(forms.ModelForm):
 
     def clean_email(self):
         email = self.cleaned_data['email']
-        if CalendallUser.objects.filter(email=email).exists():
+        if utils.email_exists(email):
             log.debug("email '{0}' already taken".format(email))
             raise forms.ValidationError(_("already taken"))
 
@@ -33,28 +34,21 @@ class CalendallUserCreateForm(forms.ModelForm):
     def clean_username(self):
         username = self.cleaned_data['username']
 
-        # Check username is: A-Z, a-z and only dashes
-        # Can't start wiht a dash
-        # 30 max
-        r = re.compile("^.(?<!\-)[a-zA-Z0-9\-]{1,29}$")
-        if not r.match(username):
+        if not utils.valid_username(username):
             log.debug("username '{0}' not ^.(?<!\-)[a-zA-Z0-9\-]{{1,29}}$".format(username))
             raise forms.ValidationError(_("May only contain alphanumeric characters or dashes and cannot begin with a dash"))
 
         # Check username exists
-        if CalendallUser.objects.filter(username=username).exists():
+        if utils.username_exists(username):
             log.debug("username '{0}' already taken".format(username))
             raise forms.ValidationError(_("already taken"))
+
         return username
 
     def clean_password(self):
         password = self.cleaned_data['password']
 
-        # Don't compile, they are simple regex
-        has_letter = re.search('[a-zA-Z]', password)
-        has_number = re.search('[0-9]', password)
-
-        if not (len(password) > 7 and has_number and has_letter):
+        if not utils.valid_password(password):
             msg = _("minimun 7 characters, one letter and one number")
             raise forms.ValidationError(msg)
 
