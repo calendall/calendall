f7e15d431adf6a06a0c3a54f38af30b5218d8a38
Xabier Larrakoetxea <slok69@gmail.com>
2015-01-15 20:49:50 +0100

    Calendall/tasks#31 Add custom js

---
+++ calendall/static/js/semantic-actions.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    // Load forms
+    $('.ui.form');
+
+    // message quit buttons
+    $('.message .close').on('click', function() {
+        $(this).closest('.message').fadeOut();
+    });
+});
