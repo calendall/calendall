be48c42d862c20b2e10661679dc2dc54eb09f5bb
Xabier Larrakoetxea <slok69@gmail.com>
2015-01-17 11:39:23 +0100

    calendall/tasks#34 Add login required mixing

--- calendall/core/views.py
+++ calendall/core/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
-# Create your views here.
+
+class LoginRequiredMixin(object):
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
