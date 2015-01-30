7dd7a7a9345c5a802e96168c78e38b824be1bb9c
Xabier Larrakoetxea <slok69@gmail.com>
2015-01-30 15:31:07 +0100

    Added basic calendar structure

--- calendall/calendars/models.py
+++ calendall/calendars/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from django.utils.encoding import python_2_unicode_compatible
 
-# Create your models here.
+from profiles.models import CalendallUser
+
+
+@python_2_unicode_compatible
+class Calendar(models.Model):
+
+    owner =  models.ForeignKey(CalendallUser)
+
+#    def __str__(self):
+#        return self.email
+#
+
+
+@python_2_unicode_compatible
+class Event(models.Model):
+
+    calendar = models.ForeignKey(Calendar)
+    # rule
+
+
+@python_2_unicode_compatible
+class Rule(models.Model):
+
+    event = models.OneToOneField(Event)
